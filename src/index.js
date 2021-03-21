2754a69050ffa97f7358be2e422cc245a652b400
Nikita Fedorchenko <nikita9972012@mail.ru>
2021-03-21 23:50:41 +0300

    header+webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,48 @@
 import './less/style.less'
 import './less/www.css'
+import './less/www.css'
 
-console.log('index.js')
+const state = {
+	width: window.innerWidth,
+	scrolled: false
+}
+
+const header = document.querySelector( 'header' )
+const nav = document.querySelector( '.navigation' )
+const burger = document.querySelector( '.burger' )
+
+
+function burgerToggle () {
+	document.body.classList.toggle( 'stop-scroll' )
+	nav.classList.toggle( 'navigation_active' )
+	burger.classList.toggle( 'burger_active' )
+}
+
+// burger
+$( '.burger' ).click( () => {
+	burgerToggle()
+} )
+
+$( '.navigation-placeholder' ).click( ( item ) => {
+	burgerToggle()
+} )
+
+
+// $$$scroll top
+document.addEventListener( 'scroll', ( e ) => {
+	const container = document.documentElement
+	if ( container.scrollTop >= 20 && !state.scrolled ) {
+		header.classList.add( 'header_scrolled' )
+		state.scrolled = true
+	} else if ( container.scrollTop < 20 && state.scrolled ) {
+		header.classList.remove( 'header_scrolled' )
+		state.scrolled = false
+	}
+} )
+
+
+// init
+
+if ( state.width >= 1000 ) {
+	// add when pc
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,84 @@
-const path = require('path');
-const MiniCss = require("mini-css-extract-plugin")
-const arguments = process.argv.slice(2).reduce((obj, item) => {
-  obj[item.split('=')[0]] = item.split('=')[1]
-  return obj
-},{})
+const path = require( 'path' );
+const MiniCss = require( "mini-css-extract-plugin" )
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' )
+const CopyWebPackPlugin = require("copy-webpack-plugin");
+const webpack = require('webpack')
 
-const mode = arguments['--mode']
-console.log(mode)
+const arguments = process.argv.slice( 2 ).reduce( ( obj, item ) => {
+	obj[item.split( '=' )[0]] = item.split( '=' )[1]
+	return obj
+}, {} )
+
+const mode = arguments['--mode'] || 'development'
+
 module.exports = {
-  context: path.join(__dirname, 'src'),
-  mode,
-  entry: './index.js',
-  output: {
-    filename: 'index.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(le|c)ss$/i,
-        use: [MiniCss.loader, "css-loader", "less-loader"]
-      }
-    ]
-  },
-  plugins: [new MiniCss()],
+	context: path.join( __dirname, 'src' ),
+	mode,
+	watchOptions: {
+		aggregateTimeout: 200,
+		poll: 1000,
+	},
+	entry: './index.js',
+	output: {
+		filename: 'index.js',
+		path: path.resolve( __dirname, 'dist' ),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(le|c)ss$/i,
+				use: [ MiniCss.loader, "css-loader", "less-loader" ]
+			},
+			{
+				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'fonts/'
+						}
+					}
+				]
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+					},
+				],
+			},
+		]
+	},
+	plugins: [
+		new MiniCss(),
+		new HtmlWebpackPlugin( {  // Also generate a test.html
+			filename: 'index.html',
+			template: '../index.html'
+		} ),
+		new CopyWebPackPlugin(
+			{
+				patterns: [{ from: path.resolve(__dirname, "src", "js"), to: "js" }],
 
+			}
+		),
+		new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery",
+			'window.$': "jquery",
+			'window.jQuery': "jquery"
+		})
+	],
+	externals: {
+		jquery: 'jQuery',
+	},
 
-  devtool: "source-map" // for debug with file names
+	devServer: {
+		contentBase: path.join( __dirname, 'dist' ),
+		compress: true,
+		port: 9000,
+		open: 'Google Chrome',
+	},
+	devtool: "source-map" // for debug with file names
 };
