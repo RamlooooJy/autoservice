66a5544074efb4a54f580b19f2a4a90536ca4676
Nikita Fedorchenko <nikita9972012@mail.ru>
2021-03-30 01:01:49 +0300

    adaptive

--- src/functions.js
+++ src/functions.js
@@ -3,67 +3,74 @@ export const tabletFrom = 768
 const nav = document.querySelector('.navigation')
 const burger = document.querySelector('.burger')
 const header = document.querySelector('header')
-
 // * global state
 export const state = {
-  width: window.innerWidth,
-  scrolled: false,
-  slickPeopleExist: true,
+	width: window.innerWidth,
+	scrolled: false,
+	slickPeopleExist: true,
+	// headerHide: {
+	// 	isHidden: false,
+	// 	lastScroll: -1,
+	// },
 	// + works * * * * * * * * * * * * *
-	worksState : {
-		selectActive: false
-	}
+	worksState: {
+		selectActive: false,
+	},
 }
+
 
 
 // +  Form banner
-export function sendBannerForm(e) {
-  e.preventDefault()
-  $.arcticmodal('close')
+export function sendBannerForm (e) {
+	e.preventDefault()
+	$.arcticmodal('close')
 }
+
+
 
 // +  toggle
-export function burgerToggle() {
-  document.body.classList.toggle('stop-scroll')
-  nav.classList.toggle('navigation_active')
-  burger.classList.toggle('burger_active')
+export function burgerToggle () {
+	document.body.classList.toggle('stop-scroll')
+	nav.classList.toggle('navigation_active')
+	burger.classList.toggle('burger_active')
 }
-
-export function setSlick(block, params = {}) {
-  const extra = {
-    infinite: false,
-    responsive: [
-      {
-        breakpoint: 999999,
-        settings: "unslick"
-      },
-      {
-        breakpoint: pcFrom,
-        settings: {
-          slidesToShow: 1,
-          centerMode:true,
-          slidesToScroll: 1,
-          variableWidth: true,
-        }
-      },
-      {
-        breakpoint: tabletFrom,
-        settings: {
+export function setSlick (block, params = {}) {
+	const extra = {
+		infinite: false,
+		responsive: [
+			{
+				breakpoint: 999999,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 1,
+					centerMode: false,
+				},
+			},
+			{
+				breakpoint: pcFrom,
+				settings: {
 					slidesToShow: 1,
-					centerMode:true,
-					centerPadding: '25px',
+					centerMode: true,
 					slidesToScroll: 1,
 					variableWidth: true,
-        }
-      },
-    ],
-    ...params,
-  }
-  if (block) {
-    block.slick(extra)
-  }
+				},
+			},
+			{
+				breakpoint: tabletFrom,
+				settings: {
+					slidesToShow: 1,
+					centerMode: true,
+					slidesToScroll: 1,
+					variableWidth: true,
+				},
+			},
+		],
+		...params,
+	}
+	if (block) {
+		block.slick(extra)
+	}
 }
-
 // * $$$scroll top
 document.addEventListener('scroll', onScroll)
 // * scroll for title function
@@ -76,9 +83,30 @@ function onScroll (e) {
 		header.classList.remove('header_scrolled')
 		state.scrolled = false
 	}
+	// if (state.headerHide.lastScroll < container.scrollTop && !state.headerHide.isHidden) {
+	// 	if (container.scrollTop - state.headerHide.lastScroll >= 10) {
+	// 		$('header').addClass('navigation-transparent')
+	// 		state.headerHide.isHidden = true
+	// 	}
+	// }
+	// if ((state.headerHide.lastScroll > container.scrollTop && state.headerHide.isHidden)
+	// 	|| container.scrollTop === 0) {
+	// 	if (container.scrollTop - state.headerHide.lastScroll <= -10 || container.scrollTop === 0) {
+	// 		$('header').removeClass('navigation-transparent')
+	// 		state.headerHide.isHidden = false
+	// 	}
+	// }
+	// state.headerHide.lastScroll = container.scrollTop
 }
-onScroll()
 
+// header.addEventListener('mouseenter', () => {
+// 	if (state.headerHide.isHidden) {
+// 		state.headerHide.isHidden = false
+// 		header.classList.remove('navigation-transparent')
+// 	}
+// })
+
+onScroll()
 
 
 // + works +  //
@@ -86,32 +114,27 @@ export function dropDownClicked (item, event) {
 	event && event.stopPropagation()
 	const dropdownNavActive = 'dropdown-app-navigation_active'
 	$('.dropdown-app-navigation').toggleClass(dropdownNavActive)
-
 	const isOpened = $('.dropdown-app-navigation').hasClass(dropdownNavActive)
-
 	state.worksState.selectActive = isOpened
 }
 export function worksSelect (clicked) {
 	$('.dropdown-app-navigation').toggleClass('dropdown-app-navigation_active')
 	const SELECTED_CLASS = 'dropdown-app-navigation-menu-item_selected'
-	const active = document.querySelector(`.${SELECTED_CLASS}`)
+	const active = document.querySelector(`.${ SELECTED_CLASS }`)
+	if (clicked === active) return
 	clicked.classList.add(SELECTED_CLASS)
-	active.classList.remove(SELECTED_CLASS)
+	active && active.classList.remove(SELECTED_CLASS)
 	$('.dropdown-app-navigation-title-label').html(clicked.innerHTML)
 }
 // ! works ! //
-
-
 // ? event click on document
-document.addEventListener('click', (e)=>{
-	if(!state.worksState.selectActive) return
+document.addEventListener('click', (e) => {
+	if (!state.worksState.selectActive) return
 	const select = $('.works .dropdown-app-navigation')[0]
-	if(!select.contains(e.target)) {
+	if (!select.contains(e.target)) {
 		dropDownClicked()
 	}
 })
-
-
 export function goTo (e, element) {
 	e.preventDefault()
 	console.log(element)
